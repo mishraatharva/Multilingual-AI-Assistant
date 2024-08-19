498ef3ca966071bbc63a074ec3037754706d1681
mishraatharva <mishraatharva825@gmail.com>
2024-08-19 10:43:46 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Atharva Mishra',
+    author_email= 'mishraatharva825@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
